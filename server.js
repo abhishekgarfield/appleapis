e8b914bdc824c810590a37765d9c4ca6b6e7d803
abhishekgarfield <abhishek2759@gmail.com>
2022-10-19 11:45:49 +0530

    userapi

--- server.js
+++ server.js
@@ -4,27 +4,26 @@ app.use(express.json());
 
 var cors=require("cors");
 app.use(cors());
-const port=process.env.PORT || 9000;
+const port=9000;
 const uri="mongodb+srv://garfield:1234@cluster0.anf24op.mongodb.net/?retryWrites=true&w=majority"
 
 var MongoClient=require("mongodb").MongoClient;
 
-/*app.get("/test",async(req,res)=>{
-    const client=new MongoClient(uri);
-    await client.connect();
-    const db=client.db("app-data");
-    const data=await db.collection("products").find().toArray();
-    res.send(data);
-})
-app.listen(9000);*/
 
+
+app.get("/test",async(req,res)=>{
+    console.log("here");
+    res.send("hi please insert data");
+})
 MongoClient.connect(uri,(err,client)=>{
     if(err) return console.log("connection failed");
-    else{
-        db=client.db("app-data");
-        app.listen(()=>{
-            console.log(`Server is running on ${port}`);
-        })
-    }
+    db=client.db("app-data");
+    app.listen(port,()=>{
+        console.log(port);
+    })
+    
+    
 })
+
+
 
